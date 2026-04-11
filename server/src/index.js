0615a9b9db4c0439dc7738a638aed6791eb3bc33
alsoshubham <shubhamsahu100100@hotmail.com>
2026-04-11 08:37:32 +0530

    feat: add static file serving and update API routing for frontend

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import config from './config/index.js';
 import { errorHandler } from './middleware/auth.js';
 
@@ -42,6 +47,13 @@ app.use('/api/reports', reportsRoutes);
 
 // Error handler
 app.use(errorHandler);
+
+// Serve Static Frontend
+app.use(express.static(path.join(__dirname, '../../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 
 // Start server
 app.listen(config.port, () => {
